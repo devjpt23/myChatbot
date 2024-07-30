68a6c4ba2d10513b564335e1015bf52ad21fb722
expectolimited <devpatel232408@gmail.com>
2024-07-30 12:23:41 +0300

    added a folder that doesnt use speech_reg module

---
+++ chatbot/mypackage/recog_whisper.py
@@ -0,0 +1,43 @@
+import whisper
+from mypackage.audio import AudioData
+import io
+import soundfile as sf
+import numpy as np
+from . import Microphone, Recognizer
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
+
+def recog_whisper(
+        audio_data, model = "base", show_dict = False, load_options = None, language = 'english', translate = False, **transscribe_options
+):
+
+    assert isinstance(audio_data, AudioData)
+
+    whisper_model = whisper.load_model(model, **load_options or {})
+
+    wav_bytes = audio_data.get_wav_data(convert_rate=16000)
+    wav_stream = io.BytesIO(wav_bytes)
+    audio_array, samplingRate = sf.read(wav_stream)
+    audio_array = audio_array.astype(np.float32)
+
+    result = whisper_model.transcribe(
+        audio_array,
+        language = language,
+        task = 'translate' if translate else None,
+        **transscribe_options
+    )
+
+    if show_dict:
+        return result
+    else:
+        return result['text']
+    
+r = Recognizer()
+with Microphone() as source:
+    r.adjust_for_ambient_noise(source,duration=0.2)
+    print("Say something ")
+    audio = r.listen(source)
+
+transcription = recog_whisper(audio, model='base') 
+print(transcription)
