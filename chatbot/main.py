833a9ffe0b5052dd0b8680c7b38f4a5a52c99612
Dev Jayesh Patel <111421987+devjpt23@users.noreply.github.com>
2024-08-01 19:37:41 +0300

    My own .recognize function (#3)
    * step one in removing speech_reg
    
    * updated .gitignore on new-fea
    
    * modified the independent file
    
    * custome recog function
    
    * changed file name
    
    * added older version of recog_whisper
    
    * added requirements file
    
    * deleted useless file
    
    * added a folder that doesnt use speech_reg module
    
    * fixed the double prompt
    
    * Ensure recog_whisper.py runs correctly without -m flag
    
    * added uncommited files
    
    * Updated the gitignore file
    
    * format main file
    
    * renamed the variable
    
    * added TODOs removed mypackage
    
    * deleted useless file
    
    ---------
    
    Co-authored-by: Dev Jayesh Patel <111421987+expectolimited@users.noreply.github.com>

--- chatbot/main.py
+++ chatbot/main.py
@@ -7,6 +7,20 @@ import time
 import warnings
 import speech_recognition as sr
 
+warnings.filterwarnings(
+    "ignore", message="FP16 is not supported on CPU; using FP32 instead"
+)
+
+
+# these are the modules for new rec function
+
+import io
+import soundfile as sf
+import numpy as np
+import whisper
+
+# we also use speech rec but, as it's shared
+
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 load_dotenv()
@@ -15,48 +29,86 @@ r = sr.Recognizer()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 messages = [
     {
-        'role': 'system',
-        'content': 'You are a pizza order-taking bot named Bob the pizza guy. Be as helpful as possible. Help customers with their orders when they seem confused, and keep your responses concise to avoid boring them. Any questions that are not related to pizza avoid them and remind user that their sole purpose is to help in pizza ordering only. Also make sure your responses are short and not too long as long prompts will waste the customer time '
+        "role": "system",
+        "content": "You are a pizza order-taking bot named Bob the pizza guy. Be as helpful as possible. Help customers with their orders when they seem confused, and keep your responses concise to avoid boring them. Any questions that are not related to pizza avoid them and remind user that their sole purpose is to help in pizza ordering only. Also make sure your responses are short and not too long as long prompts will waste the customer time ",
     }
 ]
 
+
 def getting_speech(response_text):
-    client2 = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-    response_speech = client2.audio.speech.create(model="tts-1", voice="alloy", input=response_text)
-    response_speech.stream_to_file('assistant.mp3')
-    print(f'Assistant: {response_text}')
-    
-    pygame.mixer.init()    
-    pygame.mixer.music.load('assistant.mp3')
-    pygame.mixer.music.play()    
+    openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    response_speech = openai_client.audio.speech.create(
+        model="tts-1", voice="alloy", input=response_text
+    )
+    response_speech.stream_to_file("assistant.mp3")
+    print(f"Assistant: {response_text}")
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("assistant.mp3")
+    pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         time.sleep(0.1)
     pygame.mixer.quit()
-    
+
+# TODO: Delete assitant.mp3 after use
+
+def recognize_voice(
+    audio_data,
+    model="base",
+    show_dict=False,
+    load_options=None,
+    language="english",
+    translate=False,
+    **transscribe_options,
+):
+    assert isinstance(audio_data, sr.AudioData)
+    whisper_model = whisper.load_model(model, **load_options or {})
+
+    wav_bytes = audio_data.get_wav_data(convert_rate=16000)
+    wav_stream = io.BytesIO(wav_bytes)
+    audio_array, samplingRate = sf.read(wav_stream)
+    #TODO: Remove library, get audio array in correct format for whisper using pyaudio
+    audio_array = audio_array.astype(np.float32)
+
+    result = whisper_model.transcribe(
+        audio_array,
+        language=language,
+        task="translate" if translate else None,
+        **transscribe_options,
+    )
+
+    if show_dict:
+        return result
+    else:
+        return result["text"]
+
+
 def gettingInputInVoice():
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration= 0.2)
+        # TODO: Make this work using pyaudio
+        r.adjust_for_ambient_noise(source, duration=0.2)
         print("say something... ")
         audio = r.listen(source)
-    
-    return r.recognize_whisper(audio, language='english')
+
+    # return r.recognize_whisper(audio, language='english') # this is the one with speech_recognition module
+    return recognize_voice(audio, model="base")  # this one is my own function
+
 
 while True:
     try:
 
         # user_prompt = input("user: ")
         user_prompt = gettingInputInVoice()
-        messages.append({'role': 'user', 'content': user_prompt})
+        messages.append({"role": "user", "content": user_prompt})
 
         chat_completion = client.chat.completions.create(
             messages=messages,
             model="llama3-8b-8192",
         )
         response_text = chat_completion.choices[0].message.content
-        messages.append({'role': 'assistant', 'content': response_text})
+        messages.append({"role": "assistant", "content": response_text})
 
         getting_speech(response_text)
 
     except EOFError:
         break
-
